08bb1cfce39b9bb3c22ee0e96df4a56883cbf4d4
Jack Yao <magicyao22@icloud.com>
2024-01-21 15:38:52 +0800

    Changed start_server setting.

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from pywebio import start_server
 
 if __name__ == '__main__':
     app = Application()
-    start_server(app.execute, port=PORT, debug=True, auto_open_webbrowser=True)
+    start_server(app.execute, port=PORT)
