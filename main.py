15aa6ab523ee7c13c998af782c9d3c16c2c90b32
Jack Yao <magicyao22@icloud.com>
2024-05-26 10:47:02 +0800

    Allowed remote access.

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from pywebio.platform.tornado_http import start_server
 
 if __name__ == '__main__':
     app = Application()
-    start_server(app.execute, debug=False)
+    start_server(app.execute, debug=False, remote_access=True)
