b00683bd3aa6b4b8fe2983bf9e2e296a2b103ba9
Jack Yao <magicyao22@icloud.com>
2024-05-29 21:42:21 +0800

    Added host & port.

--- configs/app_config.py
+++ configs/app_config.py
@@ -16,3 +16,8 @@ PAGE_TITLE = "♠️♥️Jack's Online Blackjack♦️♣️"
 DEALER_SLEEP = 1  # Sleep: pause before next line of code.
 EARLY_EXIT_SLEEP = 2
 GAME_END_SLEEP = 2
+
+HOST = '0.0.0.0'
+PORT = 80
+DEBUG = False
+REMOTE_ACCESS = True
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+from configs.app_config import HOST, PORT, DEBUG, REMOTE_ACCESS
 from app import Application
 from pywebio.platform.tornado_http import start_server
 
-
 if __name__ == '__main__':
     app = Application()
-    start_server(app.execute, debug=False, remote_access=True)
+    start_server(app.execute, host=HOST, port=PORT, debug=DEBUG, remote_access=REMOTE_ACCESS)
